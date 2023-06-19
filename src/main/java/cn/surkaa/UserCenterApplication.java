b5b1c1b2d4890687e62f03994d18b3c7e325b96f
SurKaa <suralcarywn@qq.com>
2023-06-19 11:01:22 +0800

    Initial Commit 并添加依赖
    SpringMVC(web)
    Mybatis
    DevTools
    JDBC
    Configuration-Processor
    Lombok
    Spring Boot Test
    Mybatis-Plus
    Junit

---
+++ src/main/java/cn/surkaa/UserCenterApplication.java
@@ -0,0 +1,13 @@
+package cn.surkaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
