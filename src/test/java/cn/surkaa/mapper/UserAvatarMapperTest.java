01c27fad9d181d0031dd0dd892bdec8afb63df5f
SurKaa <suralcarywn@qq.com>
2023-06-19 20:08:47 +0800

    测试头像表的添加和搜索

---
+++ src/test/java/cn/surkaa/mapper/UserAvatarMapperTest.java
@@ -0,0 +1,30 @@
+package cn.surkaa.mapper;
+
+import cn.surkaa.entity.UserAvatar;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserAvatarMapperTest {
+
+    @Autowired
+    private UserAvatarMapper mapper;
+
+    @Test
+    public void insertTest() {
+        UserAvatar avatar = new UserAvatar();
+        avatar.setAvatar(new byte[]{12, 117, -120});
+        int insert = mapper.insert(avatar);
+        System.out.println("insert = " + insert);
+    }
+
+    @Test
+    public void selectTest() {
+        PageDTO<UserAvatar> page = new PageDTO<>(1, 5);
+        page = mapper.selectPage(page, null);
+        System.out.println(page.getRecords());
+    }
+
+}
