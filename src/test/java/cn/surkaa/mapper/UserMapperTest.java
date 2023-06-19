c93cba14ee97f1340bc8cb758fa8eafe8bd28ce0
SurKaa <suralcarywn@qq.com>
2023-06-19 20:12:43 +0800

    测试用户表的添加和搜索

---
+++ src/test/java/cn/surkaa/mapper/UserMapperTest.java
@@ -0,0 +1,32 @@
+package cn.surkaa.mapper;
+
+import cn.surkaa.entity.User;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserMapperTest {
+
+    @Autowired
+    private UserMapper mapper;
+
+    @Test
+    public void insertTest() {
+        User user = new User();
+        user.setUserAccount("2024");
+        user.setUserPassword("");
+        int insert = mapper.insert(user);
+        System.out.println("insert = " + insert);
+    }
+
+    @Test
+    public void selectTest() {
+        IPage<User> page = new PageDTO<>(1, 5);
+        page = mapper.selectPage(page, null);
+        System.out.println(page.getRecords());
+    }
+
+}
