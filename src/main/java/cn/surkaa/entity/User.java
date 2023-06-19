4a13f1dba23a99c39c84d78ba41abc1ea2c836b4
SurKaa <suralcarywn@qq.com>
2023-06-20 00:47:12 +0800

    配置User实体类的逻辑删除

--- src/main/java/cn/surkaa/entity/User.java
+++ src/main/java/cn/surkaa/entity/User.java
@@ -1,15 +1,11 @@
 package cn.surkaa.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
-
-import lombok.Data;
 
 /**
  * 用户
@@ -84,5 +80,6 @@ public class User implements Serializable {
     /**
      * 是否删除
      */
+    @TableLogic
     private Integer isDelete;
 }
