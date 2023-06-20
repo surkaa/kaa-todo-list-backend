3e34af6289378b76de303445f3cf2245ba85c35c
SurKaa <suralcarywn@qq.com>
2023-06-20 09:08:25 +0800

    create: 添加两个请求体 登录和注册

---
+++ src/main/java/cn/surkaa/entity/request/UserLoginRequest.java
@@ -0,0 +1,23 @@
+package cn.surkaa.entity.request;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @author SurKaa
+ */
+@Data
+public class UserLoginRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3080961583213659928L;
+
+    // 账号
+    private String account;
+
+    // 密码
+    private String password;
+
+}
---
+++ src/main/java/cn/surkaa/entity/request/UserRegisterRequest.java
@@ -0,0 +1,26 @@
+package cn.surkaa.entity.request;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @author SurKaa
+ */
+@Data
+public class UserRegisterRequest implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 2822086678441354537L;
+
+    // 登录账号
+    private String account;
+
+    // 登录密码
+    private String password;
+
+    // 确认密码
+    private String checkPassword;
+
+}
