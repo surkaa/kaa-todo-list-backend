feed70a809cd2aea9e0ef891b173cd2d05320586
SurKaa <suralcarywn@qq.com>
2023-06-20 20:37:15 +0800

    UserCenterApplication添加doc

--- src/main/java/cn/surkaa/UserCenterApplication.java
+++ src/main/java/cn/surkaa/UserCenterApplication.java
@@ -3,6 +3,9 @@ package cn.surkaa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author SurKaa
+ */
 @SpringBootApplication
 public class UserCenterApplication {
 
