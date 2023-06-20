a00a978a479b8a05e915b0befaa065fe9d80ac4e
SurKaa <suralcarywn@qq.com>
2023-06-20 17:22:01 +0800

    添加异常全局捕获器

---
+++ src/main/java/cn/surkaa/exception/adive/GlobalExceptionHandle.java
@@ -0,0 +1,29 @@
+package cn.surkaa.exception.adive;
+
+import cn.surkaa.exception.LoginException;
+import cn.surkaa.exception.error.ErrorEnum;
+import cn.surkaa.module.request.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author SurKaa
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandle {
+
+    @ExceptionHandler(LoginException.class)
+    public ResponseResult<?> loginException(LoginException e) {
+        log.error("登录错误: ", e);
+        return ResponseResult.error(e.getCode(), e.getMessage(), e.getDescription());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseResult<?> exception(Exception e) {
+        log.error("Exception: ", e);
+        return ResponseResult.error(ErrorEnum.SYSTEM_ERROR);
+    }
+
+}
