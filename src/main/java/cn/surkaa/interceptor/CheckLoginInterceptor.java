646951e827be89342735cb7ee6b06f4d4628291b
SurKaa <suralcarywn@qq.com>
2023-06-20 21:06:18 +0800

    添加CheckLoginInterceptor的log

--- src/main/java/cn/surkaa/interceptor/CheckLoginInterceptor.java
+++ src/main/java/cn/surkaa/interceptor/CheckLoginInterceptor.java
@@ -25,17 +25,20 @@ public class CheckLoginInterceptor implements HandlerInterceptor {
             HttpServletResponse response,
             Object handler
     ) throws IOException {
+        log.debug("开始检测是否登录");
         try {
             Object o = request.getSession().getAttribute(LOGIN_STATE);
             User user = (User) Objects.requireNonNull(o);
             Objects.requireNonNull(user);
         } catch (Exception e) {
+            log.debug("未登录请求");
             // 未登录 但是正打算登录的直接放行
             if ("/users/login".equals(request.getRequestURI())) {
+                log.debug("因此请求正是登录请求 放行");
                 return true;
             }
             String direct = request.getContextPath() + "/users/login";
-            log.info("检测到用户未登录 请登录再尝试 将即将跳转到登陆页面: {}", direct);
+            log.info("将即将跳转到登陆页面: {}", direct);
             response.sendRedirect(direct);
             return false;
         }
