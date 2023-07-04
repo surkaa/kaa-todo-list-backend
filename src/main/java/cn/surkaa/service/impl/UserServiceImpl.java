a494669e1a98d91f62f50ed9a0cfc183cee09371
SurKaa <suralcarywn@qq.com>
2023-07-04 15:27:22 +0800

    移除impl中不必要的注释

--- src/main/java/cn/surkaa/service/impl/UserServiceImpl.java
+++ src/main/java/cn/surkaa/service/impl/UserServiceImpl.java
@@ -32,23 +32,6 @@ import static cn.surkaa.contant.UserContant.*;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         implements UserService {
 
-    /**
-     * 用户注册
-     * <h2>注册逻辑 注册条件</h2>
-     * <ul>
-     *     <li>账号密码以及确认密码都不为空(不是null 不是空字符)</li>
-     *     <li>账号长度不小于<strong>6</strong>位</li>
-     *     <li>密码不小于<strong>8</strong>位</li>
-     *     <li>账号不能以数字开头</li>
-     *     <li>密码和校验密码相同</li>
-     *     <li>账号和密码只能包含如下字符<pre>{@code a-z A-Z 0-9}</pre></li>
-     *     <li>账号不重复</li>
-     *     <li>对密码进行加密保存</li>
-     * </ul>
-     *
-     * @param registerRequest 注册请求体
-     * @return 注册成功后的用户id
-     */
     @Override
     public Long userRegister(UserRegisterRequest registerRequest) {
         log.debug("开始注册");
@@ -110,22 +93,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         return user.getId();
     }
 
-    /**
-     * 用户登录
-     * <h2>登录逻辑 登录条件</h2>
-     *
-     * <ul>
-     *     <li>账号密码都不为空(不是null 不是空字符)</li>
-     *     <li>账号长度不小于<strong>6</strong>位</li>
-     *     <li>密码不小于<strong>8</strong>位</li>
-     *     <li>账号不能以数字开头</li>
-     *     <li>账号和密码只能包含如下字符<pre>{@code a-z A-Z 0-9}</pre></li>
-     * </ul>
-     *
-     * @param loginRequest 登录请求体
-     * @param request      请求
-     * @return 脱敏后的用户信息
-     */
     @Override
     public User doLogin(UserLoginRequest loginRequest, HttpServletRequest request) {
         log.debug("开始登录");
@@ -185,20 +152,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         return safeUser;
     }
 
-    /**
-     * 根据当前用户的角色(等级)修改信息
-     *
-     * <h2>修改规则:</h2>
-     * <ul>
-     *     <li>普通用户只能修改自己的用户信息</li>
-     *     <li>管理员用户可以修改普通用户和自己的用户信息</li>
-     *     <li>超级管理员可以修改除其他超级管理员之外的用户信息</li>
-     * </ul>
-     *
-     * @param update  更改后的信息
-     * @param request 请求
-     * @return 更改成功后的信息
-     */
     @Override
     public User updateUserInfo(User update, HttpServletRequest request) {
         if (null == update) {
@@ -226,14 +179,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         return safeUser;
     }
 
-    /**
-     * 根据用户昵称搜索用户并分页
-     *
-     * @param username    用户昵称
-     * @param currentPage 当前页号
-     * @param pageSize    页大小
-     * @return 分页结果
-     */
     @Override
     public IPage<User> searchWithUserName(String username, long currentPage, long pageSize) {
         log.debug("开始通过昵称匹配用户");
